2e4596c2f4bd19628f777eea3b425729285620e5
ionarose <ionaroseuk@gmail.com>
2022-12-06 15:10:52 +0000

    cleaned up

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import questionsRouter from "./routes/questions.js"
-import topicsRouter from "./routes/topics.js"
+import questionsRouter from "./routes/questions.js";
+import topicsRouter from "./routes/topics.js";
 const app = express();
-//const PORT = 4000;
-//  var corsOptions = {
-//   origin: 'http://localhost:3000',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-app.use(cors())
-app.use(morgan("dev"));
-//app.use(express.static("./public"));
-app.use(express.json());
-app.use("/api/topics", topicsRouter)
-app.use("/api/questions", questionsRouter)
 
-  const PORT = 4000;
-  
-  app.listen(PORT, function () {
-      console.log(`Server is running on port ${PORT}`);
-    });
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/api/topics", topicsRouter);
+app.use("/api/questions", questionsRouter);
+
+const PORT = 4000;
+
+app.listen(PORT, function () {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 export default app;
